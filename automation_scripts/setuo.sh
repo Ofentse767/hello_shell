cffc1c65ff206c82e9f3105fd68d50de112e0965
Ofentse Motaung <0212176188085@tpc.local>
2025-07-28 13:56:25 +0200

    second commit

--- automation_scripts/setuo.sh
+++
@@ -1,2 +0,0 @@
-mkdir project_folder
-
--- automation_scripts/setup.sh
+++ automation_scripts/setup.sh
@@ -1,2 +1,9 @@
 mkdir project_folder
+cd project_folder
+mkdir src
+mkdir docs
+cd  src
+touch index.js
+echo "console.log('Project started');" > project_folder/src/index.js
+echo "✅ Folder structure created successfully!"
 
